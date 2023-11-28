65051c4cc98ea03553d07e07a92a5b2c951616ea
J.P <70083705+stapmoshun@users.noreply.github.com>
2023-11-28 17:06:41 -0500

    fix backend

--- website/flask_app.py
+++ website/flask_app.py
@@ -1,15 +1,32 @@
 from flask import Flask, render_template, request
+import sys
+import os
+sys.path.insert(0, '')
+from backend import trie
 
 app = Flask(__name__)
 
 @app.route('/', methods=['POST', 'GET'])
 def hello_world():
     if request.method == 'POST':
-        forward_message = "your mom..."
+        
         message_content = request.form.get('message-contents')
-        print(message_content)
-        print('we got it :)')
-        return render_template('solution.html', forward_message=forward_message);
+        hey = trie.trie()
+        hey.buildtrieFromBadWordsFile("bad-words.txt")
+        print('!!!')
+        it_is_bad = False
+        if hey.search(message_content):
+             it_is_bad = True
+        if it_is_bad:
+            return_message = 'BAD WORD! NAUGHTY! 👿'
+        else:
+            return_message = 'not a bad word you are so nice 😇'
+        # print(hey.search(message_content))
+        # print('!!!')
+        # print(message_content)
+        # print('we got it :)')
+
+        return render_template('solution.html', forward_message=return_message);
     else:
         return render_template('solution.html')
 
