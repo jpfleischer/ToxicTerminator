72b6e02633a6a380383acc7ec8b844f98ce1f876
J.P <70083705+stapmoshun@users.noreply.github.com>
2023-12-02 19:08:06 -0500

    radio button

--- website/flask_app.py
+++ website/flask_app.py
@@ -8,40 +8,53 @@ from cloudmesh.common.util import readfile
 import cppyy
 
 trie_cpp_file_contents = readfile('backend/trie_cpy.cpp')
+hash_file_contents = readfile('backend/hashmap.cpp')
 
 cppyy.cppdef(trie_cpp_file_contents)
-from cppyy.gbl import trie
-trie_obj = trie()
+cppyy.cppdef(hash_file_contents)
+
+from cppyy.gbl import trie, hashmap
+# trie_obj = trie()
 # trie_obj.buildtrieFromBadWordsFile("")
-trie_obj.main("bad-words.txt")
+# trie_obj.main("bad-words.txt")
 
 
 app = Flask(__name__)
 
-# this route points to root /
-@app.route('/', methods=['POST', 'GET'])
-def hello_world():
+@app.route('/query', methods=['POST'])
+def query():
     if request.method == 'POST':
         # message-contents contains the message that the user input into the text box.
         message_content = request.form.get('message-contents')
-        # this line imports the C++ python binding program, tree implementation.
-        trie_object = trie.trie()
-        trie_object.buildtrieFromBadWordsFile("bad-words.txt")
-        print('!!!')
-        it_is_bad = False
-        if trie_object.search(message_content):
-             it_is_bad = True
-        if it_is_bad:
-            return_message = 'BAD WORD! NAUGHTY! 👿'
-        else:
-            return_message = 'not a bad word you are so nice 😇'
-        # print(trie_object.search(message_content))
-        # print('!!!')
-        # print(message_content)
-        # print('we got it :)')
+        selected_option = request.form['data-structure']  # Get the selected radio button value
+        print('!!!!!!!!!!!!!!', selected_option)
+        if selected_option == 'trie':
+            # Handle processing with Trie
+            trie_object = trie.trie()
+            print('trie')
+            print(trie_object)
+            trie_object.buildtrieFromBadWordsFile("bad-words.txt")
+            it_is_bad = trie_object.search(message_content)
+            return_message = 'BAD WORD! NAUGHTY! 👿' if it_is_bad else 'Not a bad word! You are so nice 😇'
 
-        return render_template('solution.html', forward_message=return_message);
-    else:
-        return render_template('solution.html')
+        elif selected_option == 'hashmap':
+            # Handle processing with Hash Map or any other logic
+            # ...
+
+            hash_obj = hashmap.hashmap()
+            print("HAAAAASH!")
+            print(hash_obj)
+            hash_obj.buildHashmap("bad-words.txt")
+            it_is_bad = hash_obj.search(message_content)
+            return_message = 'BAD WORD! NAUGHTY! 👿' if it_is_bad else 'Not a bad word! You are so nice 😇'
+
+        return return_message
+
+
+# this route points to root /
+@app.route('/', methods=['POST', 'GET'])
+def hello_world():
+
+    return render_template('solution.html')
 
 
